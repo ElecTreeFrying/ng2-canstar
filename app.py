593960a153a2f769eca5824933ac244b3b3df683
alyana-garcia <alyana-garcia@gmail.com>
2018-08-26 05:44:47 -0700

    add: s/e alert

--- app.py
+++ app.py
@@ -26,4 +26,24 @@ def log():
     time = Timer(1.0, init);
     time.start();
 
+def toReturn():
+    with open('users.csv', 'w') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL);
+        filewriter.writerow(['username', '']);
+        filewriter.writerow(['password', '']);
+
+# ALERT
+def success(option, bf):
+    if option == 'signup':
+        print('Signup successful.\n\n')
+        login();
+    elif option == 'login':
+        print('Sign in successful.\n')
+        toReturn();
+    elif option == 'login_failed':
+        print('Invalid username or password.\n')
+        login();
+    else:
+        print('impossible for stack to make here.')
+
 log();
